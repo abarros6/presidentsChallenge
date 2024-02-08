35cac1362c0e4062baa531e66426b7b0bb6499f7
Curtis Floras <curtisfloras@Curtiss-MacBook-Pro.local>
2024-02-07 22:53:05 -0500

    feat: home to queue page button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react'
 import './Home.css'
 
-import { NavLink } from 'react-router-dom'
+import { NavLink, Route } from 'react-router-dom'
 
 const Home = () => {
 
+  function toQueue(e) {
+    e.preventDefault();
+    window.location.replace("/Queue");
+  }
   return (
     <div className='home_background'>
       <div className='home_main_container'>
         <div className='home_left_container'>
           <h1 className='home_main_header'>Welcome to Triage</h1>
-          <h2 className='home_sub_header'>Enter the queue now!</h2>
+          <form onSubmit={toQueue}>
+            <button>Enter the queue now!</button>
+          </form>
         </div>
       </div>
     </div>

a41a634572eaf165db2993922d5481da8330cb99
YourName <you@example.com>
2024-02-13 19:05:12 -0500

    feat: checkbox list and dropdown

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -57,18 +57,6 @@ const Form = () => {
           required
         />
       </label>
-      <label htmlFor="address" className="form-label">
-        Address:
-        <input
-          type="text"
-          id="address"
-          name="address"
-          value={formData.address}
-          onChange={handleChange}
-          className="form-input"
-          required
-        />
-      </label>
       <label htmlFor="healthCardNo" className="form-label">
         Health Card Number:
         <input
@@ -93,16 +81,30 @@ const Form = () => {
           required
         />
       </label>
+      <label htmlFor="hospital" className="form-label">
+        Choose a hospital:
+        <br></br>
+        <select id="hosp" name="hosp">
+            <option value="univ">University Hospital</option>
+            <option value="sj">St. Joseph's Hospital</option>
+            <option value="orange">Victoria Hospital</option>
+            <option value="grape">St. Thomas Elgin Memorial Hospital</option>
+        </select>
+      </label>
       <label htmlFor="symptoms" className="form-label">
-        Symptoms:
-        <textarea
-          id="symptoms"
-          name="symptoms"
-          value={formData.symptoms}
-          onChange={handleChange}
-          className="form-textarea"
-          required
-        />
+        Symptoms: <br />
+        <div class= "container">
+              <input type="checkbox" /> Cough <br />
+              <input type="checkbox" /> Fever <br />
+              <input type="checkbox" /> Chest Pain <br />
+              <input type="checkbox" /> Diarrhea <br />
+              <input type="checkbox" /> Abdominal Pain <br />
+              <input type="checkbox" /> Headache <br />
+              <input type="checkbox" /> Weight Loss <br />
+              <input type="checkbox" /> Nausea <br />
+              <input type="checkbox" /> Vomiting <br />
+              <input type="checkbox" /> Confusion <br />
+              </div>
       </label>
       <label htmlFor="medicalHistory" className="form-label">
         Medical History:
@@ -114,7 +116,7 @@ const Form = () => {
           className="form-textarea"
         />
       </label>
-      <button type="submit" className="submit-button">Submit</button>
+      <button type="submit" className="submit-button">Enter Queue</button>
     </form>
   );
 };

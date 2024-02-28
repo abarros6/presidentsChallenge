f4b09d7736e61f78b508b8bd4413d82b7b5cb7ab
Anthony <anthonyb@versa-networks.com>
2024-02-28 18:01:34 -0500

    feat: queue data color and footer rework

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.scss'
 
 const Footer = () => {
     return(
-        <div className='footer_background'>
+        <div id = "footer" className='footer_background'>
             <p className='footer_text'>President's Challenge: <a target="_blank" href='https://www.linkedin.com/in/anthony-barros-b142a1181/'>Anthony Barros </a>, <a target="_blank" href='https://www.linkedin.com/in/curtis-floras-661842208/'>Curtis Floras </a>, and <a target="_blank" href='https://www.linkedin.com/in/ethan-miranda-498a79225/'>Ethan Miranda </a></p>
             <a className='footer_item' target="_blank" href = 'https://github.com/abarros6/presidentsChallenge'>Github</a>
         </div>

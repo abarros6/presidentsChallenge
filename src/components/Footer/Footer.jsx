02acd9ecce6ec45b8a2f37f2c09320b8cd9b4991
Anthony <anthonyb@versa-networks.com>
2024-02-28 18:50:19 -0500

    feat: footer image and on hover

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Footer.scss'
+import WhiteWestern from '../../Assets/WhiteWestern.svg'
 
 const Footer = () => {
     return(
-        <div id = "footer" className='footer_background'>
+        <div id = 'footer' className='footer_background'>
             <p className='footer_text'>President's Challenge: <a target="_blank" href='https://www.linkedin.com/in/anthony-barros-b142a1181/'>Anthony Barros </a>, <a target="_blank" href='https://www.linkedin.com/in/curtis-floras-661842208/'>Curtis Floras </a>, and <a target="_blank" href='https://www.linkedin.com/in/ethan-miranda-498a79225/'>Ethan Miranda </a></p>
+            <img src={WhiteWestern} id="WhiteWestern" alt="WhiteWestern" className='fade-in-image'/>
             <a className='footer_item' target="_blank" href = 'https://github.com/abarros6/presidentsChallenge'>Github</a>
         </div>
     )
